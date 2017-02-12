39a80c248fb8b7d463e637257842fae2280780d3
Daniel Menet <membership@sontags.ch>
2017-02-12 14:36:55 +0100

    added init subcommand

---
+++ cmd/cmd_init.go
@@ -0,0 +1,80 @@
+// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/influxdata/influxdb/client/v2"
+	"github.com/spf13/cobra"
+)
+
+// vomitCmd represents the vomit command
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "creates the Database if required and sets a start date",
+	Run: func(cmd *cobra.Command, args []string) {
+		c, err := Conf()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Print("Create InfluxDB client")
+		i, err := NewInflux(c.Gulp.Host, c.Gulp.Proto, c.Gulp.Db, c.Gulp.User, c.Gulp.Pass, c.Gulp.Port)
+		if err != nil {
+			log.Print("Could not create influx client")
+			log.Fatal(err)
+		}
+
+		log.Print(fmt.Sprintf("Create InfluxDB %s", c.Gulp.Db))
+		_, err = i.Query(fmt.Sprintf("CREATE DATABASE %s", c.Gulp.Db))
+		if err != nil {
+			log.Print("Could not create database")
+			log.Fatal(err)
+		}
+
+		log.Print("Creating initial timestamp")
+
+		bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+			Database:  i.DB,
+			Precision: "s",
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Create a point and add to batch
+		tags := map[string]string{"ruminant_created": "true"}
+		fields := map[string]interface{}{
+			c.Gulp.LatestIndicator: 0,
+		}
+		dur := time.Hour * 24 * -1
+		pt, err := client.NewPoint(c.Gulp.Series, tags, fields, time.Now().Add(dur))
+		if err != nil {
+			log.Fatal(err)
+		}
+		bp.AddPoint(pt)
+
+		if err := i.Client.Write(bp); err != nil {
+			log.Fatal(err)
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(initCmd)
+}
---
+++ cmd/setup.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"bytes"
+	"html/template"
+	"log"
+)
+
+func Setup(c Config) ([]Point, Influx) {
+	log.Print("Create InfluxDB client")
+	i, err := NewInflux(c.Gulp.Host, c.Gulp.Proto, c.Gulp.Db, c.Gulp.User, c.Gulp.Pass, c.Gulp.Port)
+	if err != nil {
+		log.Print("Could not create influx client")
+		log.Fatal(err)
+	}
+
+	log.Print("Getting latest timestamp from InfluxDB")
+	latest, err := i.GetLatestInSeries(c.Gulp.Series, c.Gulp.LatestIndicator)
+	if err != nil {
+		log.Print("Could not get latest timestamp in series")
+		log.Fatal(err)
+	}
+
+	t := template.Must(template.New("t1").Parse(c.Regurgitate.Query))
+
+	var query bytes.Buffer
+	t.Execute(&query, ToEsTimestamp(latest))
+
+	log.Print("Querying ElasticSearch")
+	es := NewElasticSearch(c.Regurgitate.Proto, c.Regurgitate.Host, c.Regurgitate.Port)
+	result, err := es.Query(c.Regurgitate.Index, c.Regurgitate.Type, query.String())
+	if err != nil {
+		log.Print("Could not query ElasticSearch")
+		log.Fatal(err)
+	}
+	j, err := result.AggsAsJson()
+	if err != nil {
+		log.Fatal(err)
+	}
+	p := Point{
+		Tags:        make(map[string]string),
+		Values:      make(map[string]interface{}),
+		Measurement: c.Gulp.Series,
+	}
+	log.Print("Processing results")
+	out, err := Process(j, c.Ruminate.Iterator, p, 0)
+	if err != nil {
+		log.Print("Could not process data")
+		log.Fatal(err)
+	}
+
+	return out, i
+}
