363f938dc7f1d387dfc2a4d63718376e7f9caa22
Daniel Menet <membership@sontags.ch>
2021-08-04 16:02:39 +0200

    allow to run as lambda

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module ruminant
 go 1.13
 
 require (
+	github.com/aws/aws-lambda-go v1.26.0
 	github.com/aws/aws-sdk-go v1.35.35
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/influxdata/influxdb v1.2.3-0.20170331210902-15e594fc09f1
@@ -10,8 +11,7 @@ require (
 	github.com/nytlabs/gojee v0.0.0-20140331152115-5a79a1542dc0
 	github.com/spf13/cobra v0.0.0-20170905172051-b78744579491
 	github.com/spf13/pflag v1.0.1-0.20170901120850-7aff26db30c1 // indirect
-	github.com/stretchr/testify v1.4.0 // indirect
-	github.com/tj/go-naturaldate v1.3.0 // indirect
+	github.com/tj/go-naturaldate v1.3.0
 	go.uber.org/atomic v1.1.0 // indirect
 	go.uber.org/zap v1.0.0-rc.1.0.20170216044757-066278dd4c1d
 	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b
--- main.go
+++ main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func main() {
-	err := NewApp().Execute()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err.Error())
-		os.Exit(-1)
+	_, isLambda := os.LookupEnv("_LAMBDA_SERVER_PORT")
+	if isLambda {
+		lambda.Start(launchAsLambda)
+	} else {
+		err := launch()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR: %s\n", err.Error())
+			os.Exit(-1)
+		}
 	}
+}
+
+type LambdaEvent struct {
+	Args string `json:"args"`
+}
+
+func launchAsLambda(ctx context.Context, e LambdaEvent) (string, error) {
+	// read args from event
+	args := append([]string{os.Args[0]}, strings.Split(e.Args, " ")...)
+	os.Args = args
+
+	// run
+	err := launch()
+	return "done", err
+}
+
+func launch() error {
+	return NewApp().Execute()
 }
